f80440c0c1b3cecc4b197940e80fdbe5f176d521
Wally Jones <wally@imwally.net>
2018-04-15 23:56:34 -0400

    A whole lot of work

--- freebird.go
+++ freebird.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/dghubble/go-twitter/twitter"
 	"golang.org/x/oauth2"
@@ -14,63 +18,45 @@ import (
 var (
 	api_key    = flag.String("key", "", "API consumer key")
 	api_secret = flag.String("secret", "", "API consumer secret")
-	username   = flag.String("username", "", "Twitter user name")
+	snapshot   = flag.Bool("snapshot", false, "Print all friend's IDs to stdout")
+	unfollow   = flag.Bool("unfollow", false, "Unfollow your friends")
+	username   = flag.String("username", "", "Your user name")
 )
 
-type Friend struct {
-	ScreenName string
-	ID         int64
+func ErrAndExit(s string) {
+	fmt.Fprintf(os.Stderr, "%s: %s\n", os.Args[0], s)
+	os.Exit(1)
 }
 
-func GetFriends(client *twitter.Client, username string) ([]Friend, error) {
-	user, _, err := client.Users.Show(&twitter.UserShowParams{
-		ScreenName: username,
-	})
+func ResetSleep(resp *http.Response) {
+	if resp.Header["X-Rate-Limit-Remaining"][0] != "0" {
+		return
+	}
+
+	resetTimeStr := resp.Header["X-Rate-Limit-Reset"][0]
+	resetTime, err := strconv.ParseInt(resetTimeStr, 10, 64)
 	if err != nil {
-		return nil, err
+		log.Println(err)
 	}
 
-	params := &twitter.FriendListParams{
-		UserID:              user.ID,
-		ScreenName:          username,
-		Cursor:              -1,
-		Count:               200,
-		SkipStatus:          &[]bool{true}[0],
-		IncludeUserEntities: &[]bool{false}[0],
-	}
-
-	var friends []Friend
-	for params.Cursor != 0 {
-		f, _, err := client.Friends.List(params)
-		if err != nil {
-			return nil, err
-		}
-
-		for _, user := range f.Users {
-			friends = append(friends, Friend{user.ScreenName, user.ID})
-		}
-
-		params.Cursor = f.NextCursor
-	}
-
-	return friends, nil
+	log.Printf("%s: sleeping until %s\n", os.Args[0], time.Unix(resetTime, 0))
+	until := time.Until(time.Unix(resetTime, 0))
+	time.Sleep(until)
 }
 
 func main() {
 	flag.Parse()
 	if *api_key == "" {
-		fmt.Fprintf(os.Stderr, "%s: Please specify a Twitter API consumer key.\n", os.Args[0])
-		os.Exit(1)
+		ErrAndExit("no twitter api consumer key specified")
 	}
 	if *api_secret == "" {
-		fmt.Fprintf(os.Stderr, "%s: Please specify a Twitter API consumer secret.\n", os.Args[0])
-		os.Exit(1)
+		ErrAndExit("no twitter api consumer secret specified")
 	}
 	if *username == "" {
-		fmt.Fprintf(os.Stderr, "%s: Please specify a Twitter username.\n", os.Args[0])
-		os.Exit(1)
+		ErrAndExit("no twitter username specified")
 	}
 
+	// Generate a twitter client
 	config := &clientcredentials.Config{
 		ClientID:     *api_key,
 		ClientSecret: *api_secret,
@@ -79,11 +65,56 @@ func main() {
 	httpClient := config.Client(oauth2.NoContext)
 	client := twitter.NewClient(httpClient)
 
-	friends, err := GetFriends(client, *username)
+	// Need user's ID to generate friend list params
+	user, _, err := client.Users.Show(&twitter.UserShowParams{
+		ScreenName: *username,
+	})
 	if err != nil {
 		log.Println(err)
+	}
+
+	// Params for friend IDs
+	fp := &twitter.FriendIDParams{
+		UserID:     user.ID,
+		ScreenName: *username,
+		Cursor:     -1,
+		Count:      5000,
+	}
+
+	// Round up those friends (really, just their IDs)
+	var friendIDs []int64
+	for fp.Cursor != 0 {
+		fids, resp, err := client.Friends.IDs(fp)
+		if err != nil {
+			log.Println(err)
+		}
+		ResetSleep(resp)
+
+		for _, fid := range fids.IDs {
+			friendIDs = append(friendIDs, fid)
+		}
+
+		fp.Cursor = fids.NextCursor
+	}
+
+	// Print friend's IDs to stdout
+	if *snapshot {
+		for _, id := range friendIDs {
+			fmt.Println(id)
+		}
 		return
 	}
 
-	fmt.Println(friends)
+	// Fly away, free bird
+	if *unfollow {
+		fmt.Printf("Are you sure you want to unfollow everyone? (y/n): ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		if scanner.Text() != "y" {
+			return
+		}
+
+		fmt.Println("BOOM!")
+		return
+	}
 }
